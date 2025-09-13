6aed12b1186ea7db1b57706f794020c1c71fa159
barengmoriz <barengmoriz@gmail.com>
2025-09-13 13:28:16 +0700

    feat: create model for category, tag and post

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Post extends Model
+{
+    protected $fillable = ['title', 'slug', 'image', 'content', 'user_id', 'category_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
