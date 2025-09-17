8f8055db949a6f4007984a67a15c5fd7150e4144
barengmoriz <barengmoriz@gmail.com>
2025-09-17 16:49:47 +0700

    fix: using grid and section for ui form

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -5,9 +5,11 @@ namespace App\Filament\Resources\Posts\Schemas;
 use Illuminate\Support\Str;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\Utilities\Set;
 
 class PostForm
@@ -16,27 +18,39 @@ class PostForm
     {
         return $schema
             ->components([
-                TextInput::make('title')
-                    ->required()
-                    ->live(onBlur: true)
-                    ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state))),
-                TextInput::make('slug')
-                    ->readOnly()
-                    ->unique(),
-                FileUpload::make('image')
-                    ->image()
-                    ->required(),
-                Textarea::make('content')
-                    ->required()
-                    ->columnSpanFull(),
-                Select::make('category_id')
-                    ->relationship('category', 'name')
-                    ->required(),
-                Select::make('tags')
-                    ->relationship('tags', 'name')
-                    ->multiple()
-                    ->searchable()
-                    ->preload(),
+                Grid::make()
+                    ->columns(3)
+                    ->columnSpanFull()
+                    ->schema([
+                        Section::make()
+                            ->columnSpan(2)
+                            ->schema([
+                                TextInput::make('title')
+                                    ->required()
+                                    ->live(onBlur: true)
+                                    ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state))),
+                                TextInput::make('slug')
+                                    ->readOnly()
+                                    ->unique(),
+                                RichEditor::make('content')
+                                    ->required()
+                                    ->columnSpanFull(),
+                            ]),
+                        Section::make()
+                            ->schema([
+                                FileUpload::make('image')
+                                    ->image()
+                                    ->required(),
+                                Select::make('category_id')
+                                    ->relationship('category', 'name')
+                                    ->required(),
+                                Select::make('tags')
+                                    ->relationship('tags', 'name')
+                                    ->multiple()
+                                    ->searchable()
+                                    ->preload(),
+                            ]),
+                    ])
             ]);
     }
 }
