c5e4f9d90bbf09c187f549add1d0d0d559112dcc
barengmoriz <barengmoriz@gmail.com>
2025-09-18 14:28:09 +0700

    fix: using grid and section for ui info list

--- app/Filament/Resources/Posts/Schemas/PostInfolist.php
+++ app/Filament/Resources/Posts/Schemas/PostInfolist.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\Posts\Schemas;
 
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class PostInfolist
@@ -12,21 +14,39 @@ class PostInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('title'),
-                TextEntry::make('slug'),
-                ImageEntry::make('image'),
-                TextEntry::make('content')
-                    ->columnSpanFull(),
-                TextEntry::make('user.name')
-                    ->label('User'),
-                TextEntry::make('category.name')
-                    ->label('Category'),
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
+                Grid::make()
+                    ->columns(3)
+                    ->columnSpanFull()
+                    ->schema([
+                        Section::make()
+                            ->schema([
+                                TextEntry::make('title'),
+                                TextEntry::make('slug'),
+                                TextEntry::make('content')
+                                    ->html()
+                                    ->columnSpanFull(),
+                                TextEntry::make('category.name')
+                                    ->badge()
+                                    ->label('Category'),
+                                TextEntry::make('tags.name')
+                                    ->badge()
+                                    ->color('info')
+                                    ->label('Tag'),
+                            ])
+                            ->columnSpan(2),
+                        Section::make()
+                            ->schema([
+                                ImageEntry::make('image'),
+                                TextEntry::make('user.name')
+                                    ->label('Author'),
+                                TextEntry::make('created_at')
+                                    ->dateTime()
+                                    ->placeholder('-'),
+                                TextEntry::make('updated_at')
+                                    ->dateTime()
+                                    ->placeholder('-'),
+                            ]),
+                    ])
             ]);
     }
 }
