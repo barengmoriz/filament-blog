4b2467689eb861dd4533e38bea0a61eeb6e0e46a
barengmoriz <barengmoriz@gmail.com>
2025-09-29 12:16:27 +0700

    fix: add status to fillable and casts to PostStatusEnum

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,13 +2,21 @@
 
 namespace App\Models;
 
+use App\Enums\PostStatusEnum;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'slug', 'image', 'content', 'user_id', 'category_id'];
+    protected $fillable = ['title', 'slug', 'image', 'content', 'status', 'user_id', 'category_id'];
+
+    protected function casts(): array
+    {
+        return [
+            'status' => PostStatusEnum::class
+        ];
+    }
 
     public function user(): BelongsTo
     {
