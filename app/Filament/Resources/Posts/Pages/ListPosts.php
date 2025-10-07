11d20f07a5325490b012cebcdcd50f33dac94d39
barengmoriz <barengmoriz@gmail.com>
2025-10-07 11:09:34 +0700

    feat: create tabs for filter and show total posts on each tabs

--- app/Filament/Resources/Posts/Pages/ListPosts.php
+++ app/Filament/Resources/Posts/Pages/ListPosts.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\Posts\Pages;
 
+use App\Enums\PostStatusEnum;
 use App\Filament\Resources\Posts\PostResource;
+use App\Models\Post;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListPosts extends ListRecords
 {
@@ -14,6 +18,46 @@ class ListPosts extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function countPostsByStatus($status = 'all')
+    {
+        $posts = Post::where(function ($query) {
+            if (!auth()->user()->canAny(['Post View', 'Post Edit', 'Post Delete'])) {
+                $query->where('user_id', auth()->id());
+            }
+        });
+ 
+        if ($status == 'all') {
+            return $posts->count();
+        }
+ 
+        return $posts->where('status', $status)->count();
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make()
+                ->badge($this->countPostsByStatus())
+                ->badgeColor('primary'),
+            'draft' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', PostStatusEnum::Draft))
+                ->badge($this->countPostsByStatus(PostStatusEnum::Draft))
+                ->badgeColor('gray'),
+            'reviewing' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', PostStatusEnum::Reviewing))
+                ->badge($this->countPostsByStatus(PostStatusEnum::Reviewing))
+                ->badgeColor('info'),
+            'published' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', PostStatusEnum::Published))
+                ->badge($this->countPostsByStatus(PostStatusEnum::Published))
+                ->badgeColor('success'),
+            'rejected' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', PostStatusEnum::Rejected))
+                ->badge($this->countPostsByStatus(PostStatusEnum::Rejected))
+                ->badgeColor('danger'),
         ];
     }
 }
